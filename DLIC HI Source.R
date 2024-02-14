5a487ca8abc45e8ee2d7727c4c2cdb13aa4ea75d
Evan-Raymond <evan.raymond@gmail.com>
2024-02-15 12:02:38 +1300

    Have completed(ish) plots for DLICs and separated into 3 time periods. Will have a look at Fv/Fm readings to see if I can determine actual initial YNO readings by subtraction from Fm

--- DLIC HI Source.R
+++ DLIC HI Source.R
@@ -19,6 +19,7 @@ library(FactoMineR)
 library(corrr)
 library(ggcorrplot)
 library(corrplot)
+library(lubridate)
 options(ggrepel.max.overlaps = Inf)
 
 filter<-dplyr::filter
@@ -26,7 +27,7 @@ select<-dplyr::select
 
 detach(package:dplyr)
 
-
+install.packages("lubridate")
 
 
 # Bringing DLIC data in from "\WinControl\Heron Island\DLIC_CSV"
@@ -88,28 +89,36 @@ HI_DLIC_All<-HI_DLIC_All %>%
 
 HI_DLIC_All_1<-HI_DLIC_All %>%   
   separate_wider_delim(Treatment, "_", names = c("Treatment_1", "Sample", "Start", "Notes"), too_few = "align_start", too_many = "merge") %>% 
-  mutate(Notes = case_when(Notes == "45min_DA" ~ "45 min DA", TRUE ~ Notes)) %>% 
+  mutate(Notes = case_when(Notes == "45min_DA" ~ "45 min DA",TRUE ~ Notes)) %>%
+  replace_na(list(Notes = "-")) %>% 
   separate_wider_position(Treatment_1, c(Species = 1, Position = 1, Treatment = 1)) %>% 
   mutate(Species = case_when(Species == "O" ~ "A. aspera", Species == "I" ~ "A. muricata"),
          Position = case_when(Position == "D" ~ "Dorsal", Position == "V" ~ "Ventral"),
          Treatment = case_when(Treatment == "T" ~ "HL", Treatment == "C" ~ "ML")) %>% 
   select(-ms, -No.) %>% 
   mutate_at(c("Temp", "Start", "ETR"), .funs=as.numeric) %>%
-  mutate_at(c("Position", "Treatment"), .funs=as.factor) %>% 
+  mutate_at(c("Position", "Treatment", "Date"), .funs=as.factor) %>% 
   mutate(Temp = case_when(Temp > 100 ~ Temp / 10, TRUE ~ Temp),
          Type_1 = case_when(Species == "A. aspera" ~ "A", Species == "A. muricata" ~ "M"),
          Sun = case_when(Start %in% c(758:1110) ~ "Morning",
                          Start %in% c(1111:1400) ~ "Midday",
-                         Start %in% c(1401:1709) ~ "Afternoon")) %>% 
+                         Start %in% c(1401:1709) ~ "Afternoon"),
+         AmPm = case_when(Start %in% c(758:1230) ~ "AM",
+                          Start %in% c(1231:1709) ~ "PM"),
+         Date_1 = case_when(Date %in% c("2024-01-30","2024-01-31") ~ "1",
+                            Date %in% c('2024-02-01',"2024-02-02","2024-02-03")  ~ "2",
+                            Date %in% c("2024-02-04","2024-02-05") ~ "3")) %>% 
   group_by(Species,Position, Treatment, Sample, Start) %>% 
   mutate(SP = row_number()) %>% 
   ungroup()
-  # mutate(nYNPQ = (YNPQ*100/PAR), nYNO = (YNO*100/PAR), nYII = (YII*100/PAR)) 
+
+
+   
 
 str(HI_DLIC_All_1)
 
 HI_DLIC_All_2<-HI_DLIC_All_1 %>% 
-  group_by(Species,Position, Treatment, Sample, Start) %>% 
+  group_by(Date) %>% 
   summarise(N=n())
 
 
@@ -117,34 +126,37 @@ HI_DLIC_All_2<-HI_DLIC_All_1 %>%
 
 
 HI_DLIC_All_y_mean<-HI_DLIC_All_1 %>% 
+  filter(Notes == "-") %>% 
   pivot_longer(cols = starts_with("Y"), names_to = "Parameter", values_to = "Value") %>% 
-  group_by(Species, Position, Treatment,  SP, Parameter, Date) %>% 
+  group_by(Species, Position, Treatment,  SP, Parameter, AmPm, Date_1) %>% 
   summarise(Mean=mean(Value, na.rm=TRUE), SD=sd(Value, na.rm=TRUE), SE=SD/sqrt(n())) %>% 
   ungroup() %>% 
   mutate(Parameter = factor(Parameter, levels=c("YII","YNPQ", "YNO")),
          # Sun = factor(Sun, levels = c("Morning", "Midday", "Afternoon")),
          Treatment = factor(Treatment, levels = c("ML", "HL")),
          Position = factor(Position, levels = c("Ventral", "Dorsal"))) %>% 
-  # filter(Date != "2024-01-29")
-  filter(Date == "2024-01-31")
+  na.omit()
+
   
-HI_DLIC_All_mean_hline<- HI_DLIC_All_mean %>% 
-  filter(Parameter == "YNO", SP == 1)
+HI_DLIC_All_mean_hline<- HI_DLIC_All_y_mean %>% 
+  filter(Parameter == "YNO", SP == 1) %>% 
+  na.omit()
 
 
 HI_DLIC_All_etr_mean<-HI_DLIC_All_1 %>% 
+  filter(Notes == "-") %>% 
   pivot_longer(cols = ETR, names_to = "Parameter", values_to = "Value") %>% 
-  group_by(Species, Position, Treatment,  SP, Parameter, Date) %>% 
+  group_by(Species, Position, Treatment,  SP, Parameter, AmPm, Date_1) %>% 
   summarise(Mean=mean(Value, na.rm=TRUE), SD=sd(Value, na.rm=TRUE), SE=SD/sqrt(n())) %>% 
   ungroup() %>% 
   mutate(
          Treatment = factor(Treatment, levels = c("ML", "HL")),
          # Sun = factor(Sun, levels = c("Morning", "Midday", "Afternoon")),
          Position = factor(Position, levels = c("Ventral", "Dorsal"))) %>% 
-  # filter(Date != "2024-01-29")
-  filter(Date == "2024-01-31")
+  filter(SP != 1) %>% 
+  na.omit()
 
-coef<-550
+coef<-700
 
 
 ggplot()+
@@ -155,10 +167,11 @@ ggplot()+
   geom_hline(aes(yintercept = Mean),HI_DLIC_All_mean_hline )+
   theme_classic()+
   scale_y_continuous(expand = c(0, 0),sec.axis = sec_axis(~.*coef, name="ETR"))+
-  facet_grid(cols=vars(Date), rows=vars(Species, Position, Treatment), scales ="fixed")+
+  facet_rep_grid(cols=vars(Date_1,Treatment,AmPm), rows=vars( Species,Position), scales ="free")+
   theme(strip.placement = "outside",
-    strip.background = element_blank(),
-        strip.text.y.right= element_text(angle=0),
+        strip.background = element_blank(),
+        strip.text.y.right= element_text(angle=0), 
+        axis.title.y.right = element_text(vjust=-42),
         legend.justification = c("left"))+
   
   scale_x_continuous(expand = c(0, 0))+
